930e616a70871d32bb240d3e9cd43f5d3ea9558c
Piyush <piyushpatil0031@gmail.com>
2026-02-04 10:57:22 +0530

    SJF completed

--- Assignment_1/process_scheduling.cpp
+++ Assignment_1/process_scheduling.cpp
@@ -1,65 +1,265 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <climits>
 using namespace std;
+/*
+    1. FCFS
+    2. SJF – Preemptive and Non-preemptive
+    3. Round Robin
+    4. Priority – Preemptive and Non-preemptive
+*/
 
-class process{
-    private:
-        int p;
-        int *burst;
-        int *arrival;
-        int *waiting;
+class process
+{
+private:
+    int p;
+    int *BT;           // burst time
+    int *AT;           // arrival time
+    int *WT;           // waiting time
+    int *CT;           // completion time
+    int *RT;           // remaining time
+    int *TAT;          // turn around time
+    bool *isCompleted; // to check if process is completed
 
-    public:
-        process(){
-            p=0;
+public:
+    process()
+    {
+        p = 0;
+    }
+
+    void displayData()
+    {
+        cout << "\n================ Process Table ================\n";
+        cout << "PID\tAT\tBT\tCT\tTAT\tWT\n";
+        cout << "----------------------------------------------\n";
+
+        for (int i = 0; i < p; i++)
+        {
+            cout << i + 1 << "\t"
+                 << AT[i] << "\t"
+                 << BT[i] << "\t"
+                 << CT[i] << "\t"
+                 << TAT[i] << "\t"
+                 << WT[i] << "\n";
         }
-        
-        void displayData(){
-            cout<<"The process data is as following - \n";
-            cout<<"Process ID\t"<<"Arrival Time\t"<<"Burst Time\n";
-            for(int i=0; i<p; i++){
-                cout<<"   "<<i+1<<"\t\t  "<<arrival[i]<<"\t\t  "<<burst[i]<<"\n";
+
+        cout << "==============================================\n\n";
+    }
+
+    void inputData()
+    {
+        cout << "How many Processes are there to be executed?\n";
+        cin >> p;
+        AT = new int[p];
+        BT = new int[p];
+        WT = new int[p];
+        CT = new int[p];
+        RT = new int[p];
+        TAT = new int[p];
+        isCompleted = new bool[p];
+        cout << "Enter the AT time for each process\n";
+        for (int i = 0; i < p; i++)
+        {
+            cin >> AT[i];
+        }
+        cout << "Enter the BT time for each process\n";
+        for (int i = 0; i < p; i++)
+        {
+            cin >> BT[i];
+        }
+        cout << endl
+             << endl;
+        for (int i = 0; i < p; i++)
+        {
+            CT[i] = 0;
+            TAT[i] = 0;
+            WT[i] = 0;
+        }
+        displayData();
+    }
+
+    void FCFS()
+    {
+        WT[0] = 0;
+        cout << "ProcessID\t" << "WT Time\n";
+        cout << "   " << 1 << "\t\t" << 0 << endl;
+        for (int i = 1; i < p; i++)
+        {
+            WT[i] = BT[i - 1] + WT[i - 1];
+            // condition to check if the cpu is in ideal mode
+            if (AT[i] > WT[i])
+            {
+                WT[i] = AT[i];
             }
-            cout<<endl<<endl;
+            cout << "   " << i + 1 << "\t\t" << WT[i] << endl;
+        }
+        cout << endl
+             << endl;
+    }
+
+    void NPSJFScheduling()
+    {
+        for (int i = 0; i < p; i++)
+        {
+            isCompleted[i] = false;
         }
 
-        void inputData(){
-            cout<<"How many Processes are there to be executed?\n";
-            cin>>p;
-            arrival = new int[p];
-            burst = new int[p];
-            waiting = new int[p];
-            cout<<"Enter the arrival time for each process\n";
-            for(int i=0; i<p; i++){
-                cin>>arrival[i];
-            }
-            cout<<"Enter the burst time for each process\n";
-            for(int i=0; i<p; i++){
-                cin>>burst[i];
-            }
-            cout<<endl<<endl;
+        int time = 0, completed = 0;
 
-            displayData();
-        }
+        while (completed < p)
+        {
+            int index = -1;
+            int minBT = INT_MAX;
 
-        void calculateWaitingTime(){
-            waiting[0] = 0;
-            cout<<"ProcessID\t"<<"Waiting Time\n";
-            cout<<"   "<<1<<"\t\t\t"<<0<<endl;
-            for(int i=1; i<p; i++){
-                waiting[i] = burst[i-1]+waiting[i-1];
-                // condition to check if the cpu is in ideal mode 
-                if(arrival[i]>waiting[i]){
-                    waiting[i] = arrival[i];
+            for (int i = 0; i < p; i++)
+            {
+                if (AT[i] <= time && !isCompleted[i] && BT[i] < minBT)
+                {
+                    minBT = BT[i];
+                    index = i;
                 }
-                cout<<"   "<<i+1<<"\t\t\t"<<waiting[i]<<endl; 
-            }   
-            cout<<endl<<endl;
+            }
+
+            if (index == -1)
+            {
+                time++;
+            }
+            else
+            {
+                time += BT[index];
+                CT[index] = time;
+                TAT[index] = CT[index] - AT[index];
+                WT[index] = TAT[index] - BT[index];
+
+                isCompleted[index] = true;
+                completed++;
+            }
         }
+
+        displayData();
+    }
+
+    void PSJFScheduling()
+    {
+        for (int i = 0; i < p; i++)
+        {
+            RT[i] = BT[i]; // Remaining Time
+            isCompleted[i] = false;
+        }
+
+        int time = 0, completed = 0;
+
+        while (completed < p)
+        {
+            int index = -1;
+            int minRT = INT_MAX;
+
+            // Find process with shortest remaining time
+            for (int i = 0; i < p; i++)
+            {
+                if (AT[i] <= time && !isCompleted[i] && RT[i] < minRT)
+                {
+                    minRT = RT[i];
+                    index = i;
+                }
+            }
+
+            // CPU idle
+            if (index == -1)
+            {
+                time++;
+            }
+            else
+            {
+                RT[index]--; // execute for 1 unit
+                time++;
+
+                if (RT[index] == 0)
+                {
+                    CT[index] = time;
+                    TAT[index] = CT[index] - AT[index];
+                    WT[index] = TAT[index] - BT[index];
+
+                    isCompleted[index] = true;
+                    completed++;
+                }
+            }
+        }
+
+        displayData();
+    }
 };
 
-int main(){
-    process p;
-    p.inputData();
-    p.calculateWaitingTime();
+int main()
+{
+    int choice, subChoice;
+
+    while (true)
+    {
+        cout << "\n===== CPU Scheduling Menu =====\n";
+        cout << "1. FCFS (First Come First Serve)\n";
+        cout << "2. SJF (Shortest Job First)\n";
+        cout << "3. Round Robin\n";
+        cout << "4. Priority\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice == 5)
+        {
+            cout << "Exiting program...\n";
+            break;
+        }
+
+        process p;
+        p.inputData();
+
+        switch (choice)
+        {
+        case 1: // FCFS
+            cout << "\n--- FCFS Scheduling ---\n";
+            p.FCFS();
+            break;
+
+        case 2: // SJF
+            cout << "\n--- SJF Scheduling ---\n";
+            cout << "1. Non-Preemptive SJF\n";
+            cout << "2. Preemptive SJF\n";
+            cout << "Enter your choice: ";
+            cin >> subChoice;
+
+            if (subChoice == 1)
+                p.NPSJFScheduling();
+            else if (subChoice == 2)
+                p.PSJFScheduling();
+            else
+                cout << "Invalid choice!\n";
+            break;
+
+        case 3: // Round Robin
+            cout << "\n--- Round Robin Scheduling ---\n";
+            cout << "Round Robin not implemented yet.\n";
+            break;
+
+        case 4: // Priority
+            cout << "\n--- Priority Scheduling ---\n";
+            cout << "1. Non-Preemptive Priority\n";
+            cout << "2. Preemptive Priority\n";
+            cout << "Enter your choice: ";
+            cin >> subChoice;
+
+            if (subChoice == 1)
+                cout << "Non-Preemptive Priority not implemented yet.\n";
+            else if (subChoice == 2)
+                cout << "Preemptive Priority not implemented yet.\n";
+            else
+                cout << "Invalid choice!\n";
+            break;
+
+        default:
+            cout << "Invalid choice! Try again.\n";
+        }
+    }
+
     return 0;
 }
