10ae189baa14fa9f293b778da19864557f584f97
Piyush <piyushpatil0031@gmail.com>
2026-03-18 10:52:04 +0530

    updated code

--- Assignment_3/non_preemptive/multiple_thread/code.c
+++ Assignment_3/non_preemptive/multiple_thread/code.c
@@ -4,7 +4,7 @@
 #include <semaphore.h>
 #include <unistd.h>
 
-sem_t mutex;
+sem_t mutex, full;
 int shared_data = 0;
 
 void* producer(void* arg) {
@@ -12,19 +12,21 @@ void* producer(void* arg) {
 
     while (1) {
         int item;
-        printf("Producer Thread %d: Enter value to produce: ", id);
+
+        printf("\n[MAIN INPUT] Enter value to produce: ");
         scanf("%d", &item);
 
         sem_wait(&mutex);
 
-        printf(">>> Producer %d ENTERED critical section\n", id);
+        printf(">>> Producer %d ENTERED CS\n", id);
 
         shared_data = item;
         printf("Producer %d produced: %d\n", id, shared_data);
 
-        printf("<<< Producer %d EXITING critical section\n\n", id);
+        printf("<<< Producer %d EXITING CS\n", id);
 
         sem_post(&mutex);
+        sem_post(&full);  // signal consumer
 
         sleep(1);
     }
@@ -34,13 +36,14 @@ void* consumer(void* arg) {
     int id = *((int*)arg);
 
     while (1) {
+        sem_wait(&full);  // wait for item
         sem_wait(&mutex);
 
-        printf(">>> Consumer %d ENTERED critical section\n", id);
+        printf(">>> Consumer %d ENTERED CS\n", id);
 
         printf("Consumer %d consumed: %d\n", id, shared_data);
 
-        printf("<<< Consumer %d EXITING critical section\n\n", id);
+        printf("<<< Consumer %d EXITING CS\n", id);
 
         sem_post(&mutex);
 
@@ -51,16 +54,17 @@ void* consumer(void* arg) {
 int main() {
     int p, c;
 
-    printf("Enter number of producer threads: ");
+    printf("Enter number of producers: ");
     scanf("%d", &p);
 
-    printf("Enter number of consumer threads: ");
+    printf("Enter number of consumers: ");
     scanf("%d", &c);
 
     pthread_t prod[p], cons[c];
     int prod_id[p], cons_id[c];
 
     sem_init(&mutex, 0, 1);
+    sem_init(&full, 0, 0);  // initially empty
 
     for (int i = 0; i < p; i++) {
         prod_id[i] = i + 1;
@@ -77,8 +81,6 @@ int main() {
 
     for (int i = 0; i < c; i++)
         pthread_join(cons[i], NULL);
-
-    sem_destroy(&mutex);
 
     return 0;
 }
